c0021ddff065220bb7b83ca9f98f12445f38e563
thereclaimer <sam.davenport@thereclaimergame.com>
2026-07-09 12:17:19 -0500

    removed old configs

--- lua/config/options.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.number         = true
-vim.opt.cursorline     = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth     = 4
-vim.opt.guicursor      = "n-v-c-i:block"
-vim.opt.termguicolors  = true
-vim.opt.splitright     = true
-vim.opt.splitbelow     = true
--- lua/config/theme.lua
+++
@@ -1,2 +0,0 @@
--- Inactive windows
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "#181818" })
