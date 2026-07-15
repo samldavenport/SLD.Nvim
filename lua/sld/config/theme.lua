91a6038c62d994f0928115f68bafe747baceada7
thereclaimer <sam.davenport@thereclaimergame.com>
2026-07-15 13:02:58 -0500

    background color of inactive windows

--- lua/sld/config/theme.lua
+++ lua/sld/config/theme.lua
@@ -1,2 +1,3 @@
 vim.api.nvim_set_hl(0, "Normal",      { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#1D2021", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "NormalNC",    { bg = "#222222", ctermbg = "NONE"})
