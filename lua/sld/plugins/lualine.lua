5513ea60bef7ced0c564e6e1b1ec3fb9fb8486e3
Sam Davenport <sam.davenport@thereclaimergame.com>
2026-07-18 14:52:27 -0500

    made separators all clean blocks

--- lua/sld/plugins/lualine.lua
+++ lua/sld/plugins/lualine.lua
@@ -1,15 +1,17 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-	require('lualine').setup({
-	    options = {
-                disabled_filetypes = {
-                    statusline = {
-                        "NvimTree",
-                    },
-                },
-            },
-	})
-    end
+	'nvim-lualine/lualine.nvim',
+	dependencies = { 'nvim-tree/nvim-web-devicons' },
+	config = function()
+		require('lualine').setup({
+			options = {
+				disabled_filetypes = {
+					statusline = {
+						"NvimTree",
+					},
+				},
+				section_separators = "",
+				component_separators = "",
+			},
+		})
+	end
 }
