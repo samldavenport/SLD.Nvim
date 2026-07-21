c163dc4ad4e1f3fe9fb727d2e54c387cf8b0ce5b
Samuel Davenport <samuel.davenport@preciesely.com>
2026-07-21 10:03:02 -0500

    moving lines in insert and visual mode

--- lua/sld/config/keymaps.lua
+++ lua/sld/config/keymaps.lua
@@ -30,3 +30,7 @@ vim.keymap.set("v", "<Tab>",   ">gv")
 -- move line up/down
 vim.keymap.set("n", "<A-k>", ":m .-2<CR>==")
 vim.keymap.set("n", "<A-j>", ":m .+1<CR>==")
+vim.keymap.set("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
