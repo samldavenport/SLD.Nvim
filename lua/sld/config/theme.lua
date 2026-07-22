fd8be0b109797f1c0ffb159b7c9f7684b958b8c5
Samuel Davenport <samuel.davenport@preciesely.com>
2026-07-22 14:21:09 -0500

    Update theme.lua

--- lua/sld/config/theme.lua
+++ lua/sld/config/theme.lua
@@ -1,5 +1,5 @@
-vim.api.nvim_set_hl(0, "Normal",       { bg = "NONE",    ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "Normal",       { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat",  { bg = "NONE", ctermbg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC",     { bg = "NONE",    ctermbg = "NONE"})
-vim.api.nvim_set_hl(0, 'WinSeparator', { bg = '#3d3835', fg = '#3d3835', bold = true })
+vim.api.nvim_set_hl(0, "NormalNC",     { bg = "NONE", ctermbg = "NONE"})
+vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'NONE', fg = '#3d3835', bold = false})
 
