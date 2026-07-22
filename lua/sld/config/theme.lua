386c89cf933b026828817168e74522cb3d280f02
Samuel Davenport <samuel.davenport@preciesely.com>
2026-07-22 14:54:36 -0500

    giving original gruvbox a try

--- lua/sld/config/theme.lua
+++ lua/sld/config/theme.lua
@@ -1,5 +1,8 @@
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd.colorscheme("gruvbox")
+
 vim.api.nvim_set_hl(0, "Normal",       { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat",  { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalNC",     { bg = "NONE", ctermbg = "NONE"})
-vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'NONE', fg = '#3d3835', bold = false})
+vim.api.nvim_set_hl(0, 'WinSeparator', { bg = 'NONE',bold = false})
 
