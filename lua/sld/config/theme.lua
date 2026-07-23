bc9222d31114c61125726f9646385c21ef8f17df
Samuel Davenport <samuel.davenport@preciesely.com>
2026-07-23 08:34:17 -0500

    going back to gruvbox material

--- lua/sld/config/theme.lua
+++ lua/sld/config/theme.lua
@@ -1,5 +1,5 @@
 vim.o.background = "dark" -- or "light" for light mode
-vim.cmd.colorscheme("gruvbox")
+vim.cmd.colorscheme("gruvbox-material")
 
 vim.api.nvim_set_hl(0, "Normal",       { bg = "NONE", ctermbg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat",  { bg = "NONE", ctermbg = "NONE" })
