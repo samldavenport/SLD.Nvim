1f3af19fd8602d7267942c0de1fe12021bb80503
thereclaimer <sam.davenport@thereclaimergame.com>
2026-08-13 12:11:45 -0500

    got overseer kind of working

--- lua/sld/plugins/overseer.lua
+++ lua/sld/plugins/overseer.lua
@@ -1,6 +1,49 @@
 return {
-  'stevearc/overseer.nvim',
-  ---@module 'overseer'
-  ---@type overseer.SetupOpts
-  opts = {},
+    {
+        "stevearc/overseer.nvim",
+
+        opts = {
+            task_list = {
+                direction = "bottom",
+                min_height = 10,
+            },
+        },
+
+        config = function()
+            local overseer = require("overseer")
+            overseer.setup({
+
+            })
+
+            vim.keymap.set({"n"}, "<leader>ot", "<cmd>OverseerToggle<CR>") 
+            vim.keymap.set({"n"}, "<leader>or", "<cmd>OverseerRun<CR>") 
+        end
+    },
 }
+--[[
+
+Example project specific task
+
+overseer.register_template({
+    name = "Run Debug",
+
+    builder = function()
+        return {
+            cmd = {
+                vim.fn.getcwd() .. "/build/debug/bin/ItFliesBy.exe",
+            },
+
+            cwd = vim.fn.getcwd() .. "/build/debug/bin",
+            components = {
+                "default",
+            },
+        }
+    end,
+})
+
+vim.keymap.set("n", "<F5>", function()
+    require("overseer").run_template({
+        name = "Run Debug",
+    })
+end, {})
+--]]
