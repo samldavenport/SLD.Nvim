5dfda5b336e8c0f79a4037fcecd7bad026b20da0
Samuel Davenport <samuel.davenport@preciesely.com>
2026-08-13 20:20:15 -0500

    lazy git, telescope hidden files

--- lua/sld/plugins/telescope.lua
+++ lua/sld/plugins/telescope.lua
@@ -9,12 +9,33 @@ return {
         local telescope = require("telescope")
         local builtin = require("telescope.builtin")
 
-        telescope.setup({})
+        telescope.setup({
+            defaults = {
+                -- This affects vimgrep/live_grep arguments if needed
+                vimgrep_arguments = {
+                  "rg",
+                  "--color=never",
+                  "--no-heading",
+                  "--with-filename",
+                  "--line-number",
+                  "--column",
+                  "--smart-case",
+                  "--hidden", -- searches hidden files in live_grep
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true, -- searches hidden files in find_files
+                },
+              },
+        })
+
 
         local map = vim.keymap.set
         local opts = { silent = true }
 
         map("n", "<C-p>",      builtin.find_files, opts)
+
         map("n", "<leader>ff", builtin.find_files, opts)
         map("n", "<leader>fg", builtin.live_grep,  opts)
         map("n", "<leader>fb", builtin.buffers,    opts)
